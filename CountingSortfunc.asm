b6ed26a49c74fb7ef8d75a5b9b7aef133839c0d1
dexter196tr@gmail.com <6596321As>
2019-11-01 02:44:03 +0300

    finish the first loop

--- CountingSortfunc.asm
+++ CountingSortfunc.asm
@@ -1,6 +1,6 @@
 segment .text
 global countingsort
-
+; nasm -f elf32 csf.asm
 ; in C calling conventions
 ; ebx,esi,edi,ebp,cs,ds,ss,es should remain unchanged
 
@@ -11,3 +11,20 @@ countingsort:
 
    mov edx,[ebp+12] ; int max =a[0]
    mov ecx,[ebp+8]  ; count 
+
+MXloop:   
+   mov eax,4  ; int i = 1
+   cmp eax,ecx ; i < n n is counter 
+   jge  L1      ; end of loop one 
+   jl  MXloop      ; end of loop one 
+ 
+   
+   cmp [ebp+eax],edx ; a[i] > max      
+   jl  maxim
+
+maxim  mov edx,[ebp+eax] ; max = a[i]
+  
+   add eax,4 ; i++  
+   
+L1:   
+
